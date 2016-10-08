a833e71d092867a13504f65acefecea294f99677
Ray Dino <ray.e.dino@gmail.com>
2016-10-08 21:20:04 +0800

    Added reverse process

--- app/runspot/views.py
+++ app/runspot/views.py
@@ -36,7 +36,7 @@ class HotelsListView(TemplateView):
             'latitude': x['location']['latitude'],
             'longitude': x['location']['longitude']
         } for x in r.json()]
-        context = {'hotels': r.json()}
+        context = {'hotels': hotels}
         return context
         
 
@@ -47,21 +47,54 @@ class HotelView(TemplateView):
         hotel_id = kwargs.get('hotel_id')
         req = 'https://hacker235:pBo8BAC2Xu@distribution-xml.booking.com/' + \
               'json/bookings.getHotels?hotel_ids={}'.format(hotel_id)
-        r = requests.get(req)
-        x = r.json()[0]
+        hotel = requests.get(req)
+        x = hotel.json()[0]
         trail_req = 'https://trailapi-trailapi.p.mashape.com/?' + \
             'lat={}&lon={}&limit=25'.format(
                 x['location']['latitude'], x['location']['longitude'])
         trail = requests.get(trail_req, headers={
             'X-Mashape-Key': 'vuB0epoteBmsh0AGmCn7TMUYflKgp1sVcMPjsnA7BlatcCvEaK',
             'Accept': 'text/plain'})
+        activities = []
+        for t in trail.json()['places']:
+            activities.extend(t['activities'])
         context = {
             'name': x['name'],
             'address': x['address'],
             'hotel_id': x['hotel_id'],
             'latitude': x['location']['latitude'],
             'longitude': x['location']['longitude'],
-            'trail': trail.json()
+            'trails': activities
         }
         return context
         
+
+class CitySearchView(TemplateView):
+    template_name = 'runspot/citysearch.html'
+
+
+class TrailsListView(TemplateView):
+    template_name = 'runspot/trailslist.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        city = self.request.GET.get('city').split(',')[0]
+        city_id = self.request.GET.get('city_id')
+        trail_req = 'https://trailapi-trailapi.p.mashape.com/?' + \
+            'q[city_cont]'.format(city)
+        trail = requests.get(trail_req, headers={
+            'X-Mashape-Key': 'vuB0epoteBmsh0AGmCn7TMUYflKgp1sVcMPjsnA7BlatcCvEaK',
+            'Accept': 'text/plain'})
+        return {'city': city, 'city_id': city_id, 'trails': trail.json()['places']}
+        
+
+class TrailView(TemplateView):
+    template_name = 'runspot/trail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        lat = self.request.GET.get('lat')
+        lon = self.request.GET.get('lon')
+        req = 'https://hacker235:pBo8BAC2Xu@distribution-xml.booking.com/json/bookings.getHotels?'+\
+            'latitude={}&longitude={}'.format(lat, lon)
+        r = requests.get(req)
+        print(r.json())
+        return {'trail': self.request.GET, 'hotels': r.json()} 
